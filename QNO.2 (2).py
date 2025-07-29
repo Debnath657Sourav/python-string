081d1a042fb1d4b0f7fa95a2a6c5ea79177ee2d7
Debnath657Sourav <souravdebnath1106@gmail.com>
2025-07-29 19:13:21 +0530

    Largest-element-in-array4009/1

---
+++ QNO.2 (2).py
@@ -0,0 +1,9 @@
+def largestElement(arr):
+    max_val = arr[0]       # Assume first element is largest
+    for num in arr[1:]:
+        if num > max_val:
+            max_val = num
+    return max_val
+
+arr = [10, 20, 4, 45, 99]
+print("Largest element:", largestElement(arr))
