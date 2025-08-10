30b1092f953a47a8eaf9a5217cb92759c775869f
Debnath657Sourav <souravdebnath1106@gmail.com>
2025-08-10 23:34:16 +0530

    Update and rename QNO.3.py to QNO.2.py
     Insert in middle of linked list

--- week3/QNO.3.py
+++
@@ -1,8 +0,0 @@
-def reverse_doubly_linked_list(head):
-    if not head: return None
-    curr = head
-    while curr:
-        curr.prev, curr.next = curr.next, curr.prev
-        head = curr
-        curr = curr.prev
-    return head
